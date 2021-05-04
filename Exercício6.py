cb13004e3ac7a6e6f856cdce40afc1830d2dd2ee
Paulo Henrique <83513847+sudo-lupus666@users.noreply.github.com>
2021-05-04 20:18:48 -0300

    Update Exercício6.py

--- Exercício6.py
+++ Exercício6.py
@@ -3,11 +3,11 @@ def exercicio6():
     print("Programa que calcula a área do círculo")
     raio = int(input("Informe o raio do círculo: "))
 
-    def calcula_raio(raio):
+    def calcula_raio():
         raioaoquadrado = raio * raio
-        area = 3,14 * raioaoquadrado
+        area = 3.14 * raioaoquadrado
         return area
     
-    print(f'A área de um círculo de raio {raio} é igual a {calcula_raio()}")
+    print(f'A área de um círculo de raio {raio} é igual a {calcula_raio()}')
 
 exercicio6()
