05b12561352e5ea67318d26d7c1831d65cbb1398
Paulo Henrique <83513847+sudo-lupus666@users.noreply.github.com>
2021-05-05 19:27:38 -0300

    Exercicio10

---
+++ Exercicio 10.py
@@ -0,0 +1,17 @@
+#Faça um Programa que peça a temperatura em 
+#graus Celsius, transforme e mostre em graus Fahrenheit.
+
+def conversor_Celsius>Fahrenheit():
+    print ("**Bem-vindo ao conversor de Celsius para Fahrenheit**")
+    temp_celsius = int(input("Insira a temperatura que você deseja converter de Celsius para Fahrenheit: "))
+
+    def conversor():
+        passo1 = (temp_celsius / 5)
+        passo2 = passo1 / 9
+
+
+        100 = ((F-32)/9)
+        100*9 = F-32
+        900 = F -32
+        F = 900 + 32
+        F = 932
