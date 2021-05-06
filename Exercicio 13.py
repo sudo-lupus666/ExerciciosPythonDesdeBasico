d01d566127b432257f3be51a2673c834a6782dc7
lupinho123 <henrique.wide@gmail.com>
2021-05-06 09:09:49 -0300

    Create Exercicio 13.py

---
+++ Exercicio 13.py
@@ -0,0 +1,17 @@
+#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, 
+# usando a seguinte fórmula: (72.7*altura) - 58
+
+def pesoIdeal():
+
+    print ("**Programa para informar o seu peso ideal**")
+
+    altura = int(input("Informe a sua altura: "))
+
+    def calcula_peso_ideal():
+        peso_ideal = altura * 72,7
+        peso_ideal_passo2 = 
+        return peso_ideal
+
+    print (f'Considerando a altura de {altura} metros, o seu peso ideal é {calcula_peso_ideal()}')
+
+pesoIdeal()
