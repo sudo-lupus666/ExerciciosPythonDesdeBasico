23dbf4aa31928c2b5ba79a55f558140965b40f88
lupinho123 <henrique.wide@gmail.com>
2021-05-12 14:59:23 -0300

    Create Decisões - 3_erro.py

---
+++ Decisões - 3_erro.py
@@ -0,0 +1,24 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - 
+#Feminino, M - Masculino, Sexo Inválido.
+
+def verifica_sexualidade(): 
+
+    print ("**Programa que verifica o input de um user e determina se esse user é homem ou mulher**")
+    identificacao = (input("**Por favor, digite 'f' para sexo feminino ou 'm' para sexo masculino: **")).lower()
+    
+    def validador_input():
+        if identificacao == 'f' or 'm': return True
+        else: return False
+
+    def define_sexo():
+        if validador_input() == True:
+            if identificacao == 'f': return 'feminino'
+            else: return 'masculino'
+        else: print ('Opção inválida')  
+
+    def resposta():
+        print (f' De acordo com o que você nos infornou, você é uma pessoa do sexo {define_sexo()}')
+
+    resposta()
+
+verifica_sexualidade()
