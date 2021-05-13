ad1efd39ecc537587d67fbb64fdc6e3ecd33a2ab
lupinho123 <henrique.wide@gmail.com>
2021-05-13 19:55:54 -0300

    Create Decisões - 6.py

---
+++ Decisões - 6.py
@@ -0,0 +1,21 @@
+#Faça um Programa que leia três números e mostre o maior deles.
+
+    
+    from decimal import *
+
+def analisaNumero():
+
+    print ("**Programa - lê 3 números e devolve o maior**")
+
+    numero1 = float(input("Informe o primeiro número: "))
+    numero2 = float(input("Informe o segundo número: "))
+    numero3 = float(input("Informe o terceiro número: "))
+
+    def verificaMaior():
+    numeros = [numero1, numero2, numero3]
+    numeros.sort 
+   
+
+    print (verificaAprovacao())
+
+aprovadoReprovado()
