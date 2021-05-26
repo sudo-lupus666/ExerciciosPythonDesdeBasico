6603f03506eadf80ebf25012bb9e5bb55511267f
Paulo Henrique <83513847+sudo-lupus666@users.noreply.github.com>
2021-05-26 20:54:15 -0300

    Update Decisões - 11.py

--- Decisões - 11.py
+++ Decisões - 11.py
@@ -1,24 +1,24 @@
-'''As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe 
-contraram para desenvolver o programa que calculará os reajustes.
-Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, 
-baseado no salário atual:
-salários até R$ 280,00 (incluindo) : aumento de 20%
-salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
-salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
-salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
-o salário antes do reajuste;
-o percentual de aumento aplicado;
-o valor do aumento;
-o novo salário, após o aumento.'''
+#As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe 
+#contraram para desenvolver o programa que calculará os reajustes.
+#Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, 
+#baseado no salário atual:
+#alários até R$ 280,00 (incluindo) : aumento de 20%
+#salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+#salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+#salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+#o salário antes do reajuste;
+#o percentual de aumento aplicado;
+#o valor do aumento;
+#o novo salário, após o aumento.
 
-import decimal
+from decimal import *
 
 def calcula_salario():
 
     print("** Programa que calcula os novos salários dos funcionários das organizações Tabajara**")
     
     def entrada():
-        salario_atual = input(float("Informe o salário atual do funcionário: "))
+        salario_atual = float(input("Informe o salário atual do funcionário: "))
         return salario_atual
 
     def calcula_aumento():
@@ -31,17 +31,20 @@ def calcula_salario():
 
     def porcentagem_aumento():
         salario = entrada()
-        if salario <= 280: mensagem_aumento = f'você recebeu um aumento de 20%'
-        elif salario > 280 and salario < 700: mensagem_aumento = f'você recebeu um aumento de 15%'
-        elif salario >= 700 and salario < 1500: mensagem_aumento = f'você recebeu um aumento de 10%'
-        else: mensagem_aumento = f'você recebeu um aumento de 05%'
+        if salario <= 280: mensagem_aumento = '20%'
+        elif salario > 280 and salario < 700: mensagem_aumento = '15%'
+        elif salario >= 700 and salario < 1500: mensagem_aumento = '10%'
+        else: mensagem_aumento = '05%'
         return mensagem_aumento
 
     def mensagem():
-        return f'Prezado funcionário, o seu salário anterior era de {entrada}' 
+        return (
+            f'Prezado funcionário, o seu salário '
+            f'anterior era de R$ {entrada()}, o seu aumento foi de R$ {calcula_aumento()} e '
+            f'a porcentagem do seu aumento foi de {porcentagem_aumento()}. O seu novo salário é '
+            f' {entrada() + calcula_aumento()}'
+        )
 
-#    while entrada() != "m" or entrada() != "v" or entrada() != "n": entrada()
     print (mensagem())
 
-turno_estudo()
-
+calcula_salario()
