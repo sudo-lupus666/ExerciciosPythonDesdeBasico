c4c91e51628a1e554a9a6a1f3a972864ac9768d0
lupinho123 <henrique.wide@gmail.com>
2021-06-22 15:28:38 -0300

    teste

--- Renan_FlipTheCoin.py
+++ Renan_FlipTheCoin.py
@@ -1,14 +1,15 @@
 import random
+
 def flipTheCoin():
 
     def jogaMoeda():
         elementoRandomico = (random.randrange(2,4,1)) % 2
         pares = 2 % 1               
-        if elementoRandomico == pares: return "cara"
-        else: return "coroa"
+        if elementoRandomico == pares: return "Heads"
+        else: return "Tails"
 
     def mensagemUser():
-        return f"Nessa jogada, deu {jogaMoeda()}."
+        return f"You've got {jogaMoeda()}."
     
     print (mensagemUser())
 
