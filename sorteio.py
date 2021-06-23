ed9c1086eaba99a59d6c367a3630b0acf6631c5a
lupinho123 <henrique.wide@gmail.com>
2021-06-23 11:49:52 -0300

    Update sorteio.py

--- sorteio.py
+++ sorteio.py
@@ -1,22 +1,45 @@
 import random
 
+'''def entrada():
+    lista = []
+    print ('Entre com a lista de alunos que participarão da competição')
+    alunos = input('Insira a lista de alunos: ')
+    for nome in alunos:
+        lista.append(nome)
+    return lista'''
+
 def entrada():
-lista = []
-aluno = input('Entre com a lista de alunos que participarão da competição: .Quando terminar, digite "fim"')
-while aluno != 'fim':
-    lista.append(aluno)
-return lista
+    lista = []
+    print ('Entre com a lista de alunos que participarão da competição. Quando terminar, digite "fim"')
+    aluno = input('Insira o nome do primeiro aluno: ')
+    while aluno != 'fim':
+        lista.append(aluno)
+        aluno = input ('Insira o nome do próximo aluno: ')
+    return lista 
 
-deflista_de_participantes = entrada()
+lista_de_participantes = entrada()
+tamanho_lista = len(lista_de_participantes) -1
 
-tamanho_lista = len(lista)
+def sorteio1():
+    tamanho_lista = len(lista_de_participantes)
+    randomico = random.randrange(0,tamanho_lista,1)
+    sorteado1 = lista_de_participantes[randomico]
+    lista_de_participantes.remove(sorteado1)
+    return sorteado1
 
-def sorteio():
-    lista = [lista_de_alunos]
-    randomico = random.randrange(1,tamanho_lista,1)
-    sorteado = lista(randomico)
-    lista.remove(sorteado)
-    return sorteado
+tamanho_lista = len(lista_de_participantes)
 
-def mensagem_user:
-    print (f"O aluno 1 é o(a) {sorteio()} 
+def sorteio2():
+    tamanho_lista = len(lista_de_participantes)
+    randomico = random.randrange(0,tamanho_lista,1)
+    sorteado2 = lista_de_participantes[randomico -1]
+    lista_de_participantes.remove(sorteado2)
+    return sorteado2
+
+sorteado1 = sorteio1()
+sorteado2 = sorteio2()    
+
+def mensagem_user():
+    return f'O aluno 1 é o(a) {sorteado1}. O participante 2 é o(a) {sorteado2}'
+
+print (mensagem_user())
